213adbd8de088206c24923dff762269ae2ec3ee7
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-12-28 00:12:57 +0530

    tps setup

--- src/Components/Plane.tsx
+++ src/Components/Plane.tsx
@@ -1,8 +1,7 @@
 
 
-type Props = {}
 
-const Plane = (props: Props) => {
+const Plane = () => {
   return (
     <mesh position={[0,-2,0]} rotation={[-Math.PI/2,0,0]}>
         <planeGeometry args={[10,10]}/>
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,8 +1,7 @@
 import Plane from "./Components/Plane";
 import Player from "./Components/Player";
 import CameraRig from "./Components/CameraRig"
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
+
 
 
 const Scene = () => {
